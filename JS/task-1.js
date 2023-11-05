84e7110f97fb01017d3fbd40dcc67bc86d6ef2cb
Vishnya322 <Vishnya322@gmail.com>
2023-11-05 23:43:26 +0200

    hw-4

---
+++ JS/task-1.js
@@ -0,0 +1,28 @@
+function isEnoughCapacity(products, containerSize) {
+    const values = Object.values(products);
+  let total = 0;
+  for (let value of values) {
+  total += value;}
+    if(total <= containerSize) {
+        return true;
+    }
+  return false;
+}
+
+
+
+
+console.log(
+isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+  
+  console.log(
+    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+  );
+  
+  console.log(
+    isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
+  );
+  
+  console.log(
+    isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
+  ); 
